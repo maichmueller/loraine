76c603f958ffffed4910456263281787e4af6c62
Michael <m.aichmueller@gmail.com>
2020-07-14 11:26:52 +0200

    remove zlib install and catch2 refs

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,14 +1,2 @@
-#include <catch2/catch.hpp>
+#include <gtest/gtest.h>
 
-unsigned int Factorial(unsigned int number)
-{
-  return number <= 1 ? number : Factorial(number - 1) * number;
-}
-
-TEST_CASE("Factorials are computed", "[factorial]")
-{
-  REQUIRE(Factorial(1) == 1);
-  REQUIRE(Factorial(2) == 2);
-  REQUIRE(Factorial(3) == 6);
-  REQUIRE(Factorial(10) == 3628800);
-}
